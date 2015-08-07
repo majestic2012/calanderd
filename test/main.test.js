ab72b1b2c3b4d8651c6d81be274ffe4314b64603
curtiszimmerman <curtis.zimmerman@gmail.com>
2015-08-07 10:32:41 -0700

    added: tests, fixed ebkac with mode parser

--- test/main.test.js
+++ test/main.test.js
@@ -2,15 +2,18 @@ var __test = (function() {
 	"use strict";
 
 	var ivo = require(__dirname+'/../main.js');
+	console.log('=====['+JSON.stringify(ivo)+']')
 	var $ivo = ivo.__test;
+	var func = $ivo.func;
 	
 	var expect = require('chai').expect;
 	
-	describe('ivo schwarz', function() {
-		describe('# thing', function() {
-			it('should be awesome', function() {
-				expect(true).to.equal(true);
+	describe('ivo schwarz functions', function() {
+		describe('# __dev.getEvents', function() {
+			it('should return an array of six default objects', function() {
+				expect(func.__dev.getEvents()).to.be.an('array').and.to.have.length(6);
 			});
+			it('should return an array containing six ob')
 		});
 	});
 })();
